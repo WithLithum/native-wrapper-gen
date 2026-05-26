754be4f69ec9fedcfb0c9d11ee0c7aef55544e9b
WithLithum <WithLithum@outlook.com>
2026-05-26 22:19:30 +0800

    👷 build(wrappers): add no restore for each wrapper build
    Signed-off-by: WithLithum <WithLithum@outlook.com>

--- GenerateNativeWrapper.ps1
+++ GenerateNativeWrapper.ps1
@@ -70,6 +70,7 @@ function Write-Wrappers {
         "$csprojPath" `
         --configuration Release `
         --nologo `
+        --no-restore `
         --output $outPath `
         --verbosity minimal
 
