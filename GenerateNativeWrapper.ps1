6c89d38b4435a91d79570a1eca0c9f787871443a
WithLithum <WithLithum@outlook.com>
2026-05-26 17:18:06 +0800

    🔨 script(wrapper): make wrapper build script to generate release archive
    Signed-off-by: WithLithum <WithLithum@outlook.com>

--- GenerateNativeWrapper.ps1
+++ GenerateNativeWrapper.ps1
@@ -22,16 +22,68 @@ if (!($NoDownload)) {
     }
 }
 
+$artefactPath = Join-Path -Path $PWD -ChildPath "NWG_WORK\bin"
+$binPath = Join-Path -Path $PWD -ChildPath "bin"
+
+# Ensure bin exists
+if (!(Test-Path $binPath -PathType Container))
+{
+    New-Item -Path $PWD -Name "bin" -ItemType Directory
+}
+
 function Write-Wrappers {
     param (
         [string]$Generator,
         [string]$Project
     )
 
-    $wrapperPath = Join-Path -Path $PWD -ChildPath "wrappers\$project" -AdditionalChildPath "Natives.{0}.cs"
+    $projectPath = Join-Path -Path $PWD -ChildPath "wrappers\$project"
+    $wrapperPath = Join-Path -Path $projectPath -ChildPath "Natives.{0}.cs"
 
-    dotnet run --project "WithLithum.NativeWrapperGen" --configuration Release -v q -- --natives-file "${nativesJsonPath}" --namespace WithLithum.NativeWrapper --class-name Natives --generator "$Generator" --file-name-format "$wrapperPath" --count-time
+    dotnet run `
+    --project "WithLithum.NativeWrapperGen" `
+    --configuration Release `
+    -v q `
+    -- `
+    --natives-file "${nativesJsonPath}" `
+    --namespace WithLithum.NativeWrapper `
+    --class-name Natives `
+    --generator "$Generator" `
+    --file-name-format "$wrapperPath"
+
+    if (!$?) {
+        Write-Host -ForegroundColor Red -Object "Generator for $Project ($Generator) failed"
+        Exit 1
+    }
+
+    # Execute build
+    $csprojPath = Join-Path -Path $projectPath -ChildPath "$($project).csproj"
+    $outPath = Join-Path -Path $artefactPath -ChildPath "$generator"
+
+    dotnet build `
+    "$csprojPath" `
+    --configuration Release `
+    --nologo `
+    --output $outPath `
+    --verbosity minimal
+
+    if (!$?) {
+        Write-Host -ForegroundColor Red -Object "Build for $Project ($Generator) failed"
+        Exit 1
+    }
+
+    # Move nuget packages
+    Move-Item -Path "$outPath\*.nupkg" -Destination $binPath -Force
+
+    Write-Output "Created wrappers binaries at $hooksPath"
 }
 
 Write-Wrappers -Generator "shvdn" -Project "WithLithum.NativeWrapper"
 Write-Wrappers -Generator "rph" -Project "WithLithum.NativeWrapper.RagePluginHook"
+
+# Create final archive
+Copy-Item -Path ".\wrappers\WrapperDistReadme.txt" -Destination "$artefactPath\README.txt" -Force
+Compress-Archive -Path "$artefactPath\*" -DestinationPath "$binPath\NativeWrapper.zip" -Force
+
+# Offer to open bin path
+Write-Output "Success: created outputs at: $binPath"
